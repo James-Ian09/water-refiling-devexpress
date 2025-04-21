7acc279a43f815b2b28f1e7fa4df2ecba4964202
Pague, Lloyd <kristianpague@gmail.com>
2025-04-21 15:58:46 +0800

    Created Properties for the Customer class......

--- WaterRefillingStationSystem/Models/Customers.cs
+++ WaterRefillingStationSystem/Models/Customers.cs
@@ -8,6 +8,23 @@ namespace WaterRefillingStationSystem.Models
 {
     public class Customers
     {
-        public int MyProperty { get; set; }
+        public string ID { get; set; }
+        public string FirstName { get; set; }
+        public string MiddleName { get; set; }
+        public string LastName { get; set; }
+        public string ContactNumber { get; set; }
+        public string Address { get; set; }
+        public int Debt { get; set; }
+
+        public Customers(string iD, string firstName, string middleName, string lastName, string contactNumber, string address, int debt)
+        {
+            ID = iD;
+            FirstName = firstName;
+            MiddleName = middleName;
+            LastName = lastName;
+            ContactNumber = contactNumber;
+            Address = address;
+            Debt = debt;
+        }
     }
 }
