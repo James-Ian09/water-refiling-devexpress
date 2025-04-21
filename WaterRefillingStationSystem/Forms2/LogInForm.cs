0772b79fa52dde846da93548024fdfed089ba455
James-Ian09 <jamesianbayonas21@gmail.com>
2025-04-21 09:37:44 +0800

    Adding Login Forms

--- WaterRefillingStationSystem/Forms2/LogInForm.cs
+++ WaterRefillingStationSystem/Forms2/LogInForm.cs
@@ -1,13 +1,5 @@
 ﻿using DevExpress.XtraEditors;
 using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows.Forms;
 
 namespace WaterRefillingStationSystem.Forms
 {
@@ -17,25 +9,24 @@ namespace WaterRefillingStationSystem.Forms
         {
             InitializeComponent();
         }
-
         private void simpleButtonLogIn_Click(object sender, EventArgs e)
         {
-            //string userName = textEditUserName.Text;
-            //string password = textEditPassword.Text;
+            string userName = textEditUserName.Text;
+            string password = textEditPassword.Text;
 
-            //if(userName != "Admin" || password != "Admin123")
-            //{
-            //    XtraMessageBox.Show("Invalid username or password. Please try again.");
-            //}
-            //else
-            //{
-            //    MainForm2 mainForm2 = new MainForm2();
-            //    mainForm2.Show();
-            //    this.Hide();
-            //}
-            MainForm2 mainForm2 = new MainForm2();
-            mainForm2.Show();
-            this.Hide();
+            if(userName != "Admin" || password != "Admin123")
+            {
+                XtraMessageBox.Show("Invalid username or password. Please try again.");
+            }
+            else
+            {
+                MainForm mainForm2 = new MainForm();
+                mainForm2.Show();
+                this.Hide();
+            }
         }
+        private void textEditUserName_EditValueChanged(object sender, EventArgs e)
+        { }
+        
     }
 }
